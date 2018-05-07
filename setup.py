7f924675a3bc4467fa39adf8e9e677f1f8d8fa19
Jan-Gerd Tenberge <janten@gmail.com>
2018-05-07 19:25:19 +0200

    Version 0.4

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 setup(
   name = 'iptk',
-  packages = ['iptk'],
-  version = '0.3',
+  packages = ['iptk', 'iptk.metadata', 'iptk.utils'],
+  version = '0.4',
   description = 'Python interface to the Imaging Pipeline Toolkit',
   author = 'Jan-Gerd Tenberge',
   author_email = 'jan-gerd.tenberge@uni-muenster.de',
@@ -11,7 +11,7 @@ setup(
     'requests'
   ],
   url = 'https://github.com/iptk/iptk-py',
-  download_url = 'https://github.com/iptk/iptk-py/archive/0.3.tar.gz', 
+  download_url = 'https://github.com/iptk/iptk-py/archive/0.4.tar.gz', 
   keywords = ['neuroimaging', 'docker'],
   classifiers = [],
 )
